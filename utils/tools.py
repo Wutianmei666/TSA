778099acafac0184cebd4db96874708a7a165f91
Wutianmei666 <1461304613@qq.com>
2024-07-10 15:46:48 +0800

    a

--- utils/tools.py
+++ utils/tools.py
@@ -59,6 +59,38 @@ class EarlyStopping:
         torch.save(model.state_dict(), path + '/' + 'checkpoint.pth')
         self.val_loss_min = val_loss
 
+class EarlyStopping_J:
+    def __init__(self, patience=7, verbose=False, delta=0):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+
+    def __call__(self, val_loss, imp_model, ds_model,path):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, imp_model, ds_model, path)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, imp_model, ds_model, path)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, imp_model, ds_model, path):
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        state = {'imp_model':imp_model.state_dict(),
+                 'ds_model':ds_model.state_dict()}
+        torch.save(state, path + '/' + 'checkpoint.pth')
+        self.val_loss_min = val_loss
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
