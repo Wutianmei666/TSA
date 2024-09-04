e630832a8925ff974901463897a8939386c0bab0
Wutianmei666 <1461304613@qq.com>
2024-09-04 21:49:44 +0800

    ..

--- test.sh
+++ test.sh
@@ -1,165 +1,46 @@
-export CUDA_VISIBLE_DEVICES=0
-
-model_name=Transformer
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_mask_0.125 \
-  --mask_rate 0.125 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 64 \
-  --d_ff 64 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_mask_0.25 \
-  --mask_rate 0.25 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 64 \
-  --d_ff 64 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_mask_0.375 \
-  --mask_rate 0.375 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 64 \
-  --d_ff 64 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_mask_0.5 \
-  --mask_rate 0.5 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 64 \
-  --d_ff 64 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_mask_0.625 \
-  --mask_rate 0.625 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 64 \
-  --d_ff 64 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
-
-  python -u run.py \
-  --task_name imputation \
-  --is_training 1 \
-  --root_path ./dataset/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_mask_0.75 \
-  --mask_rate 0.75 \
-  --model $model_name \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 0 \
-  --pred_len 0 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --batch_size 16 \
-  --d_model 64 \
-  --d_ff 64 \
-  --des 'Exp' \
-  --itr 1 \
-  --top_k 5 \
-  --learning_rate 0.001
+export CUDA_VISIBLE_DEVICES=1
+model_name=TimesNet
+imp_args_json=ImpModelArgs/ETT/Transformer_ETTh1.json
+imp_lr=0.001
+seq_len=96
+label_len=48
+pred_len=96
+e_layers=2
+d_layers=1
+d_model=32
+d_ff=32
+learning_rate=0.0001
+top_k=5
+for mask_rate in 0.125 
+do
+    python -u mix_run.py \
+      --task_name long_term_forecast \
+      --train_mode 1 \
+      --_lambda 0.5 \
+      --requires_grad \
+      --imp_args_json $imp_args_json \
+      --mask_rate $mask_rate \
+      --imp_lr $imp_lr \
+      --is_training 1 \
+      --learning_rate $learning_rate \
+      --root_path ./dataset/ETT-small/ \
+      --data_path ETTh1.csv \
+      --model_id ETTh1_${mask_rate}_96_96_J \
+      --model $model_name \
+      --data ETTh1 \
+      --features M \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers $e_layers \
+      --d_layers $d_layers \
+      --d_model $d_model \
+      --d_ff $d_ff \
+      --top_k $top_k \
+      --factor 3 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --itr 1 
+done
