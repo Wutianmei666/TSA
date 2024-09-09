4cfa64b81533872d2f7439af266c337c37ec5599
XiAnServer3 <1461304613@qq.com>
2024-09-09 19:16:00 +0800

    9.9 update

---
+++ ETTm1_imp.sh
@@ -0,0 +1,40 @@
+export CUDA_VISIBLE_DEVICES=0
+model_name=Autoformer
+d_model=128
+d_ff=128
+seq_len=96
+label_len=0
+pred_len=0
+learning_rate=0.001
+for mask_rate in 0.125 0.25 0.375 0.5 0.625 0.75
+do
+python -u run.py \
+  --task_name imputation \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm1.csv \
+  --model_id ETTm1_mask_${mask_rate} \
+  --mask_rate $mask_rate \
+  --model $model_name \
+  --data ETTm1 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len $label_len \
+  --pred_len $pred_len \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --batch_size 16 \
+  --d_model $d_model \
+  --d_ff $d_ff \
+  --des 'Exp' \
+  --itr 1 \
+  --top_k 5 \
+  --learning_rate $learning_rate\
+  --lradj type1 \
+  --train_epoches 10
+done 
+
