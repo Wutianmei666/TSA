f89599baef2ebeff1017c1f6ac5ef1c0ae30a87e
XiAnServer3 <1461304613@qq.com>
2024-09-19 14:57:56 +0800

    update

--- impute_all.sh
+++ impute_all.sh
@@ -1,4 +1,4 @@
-# bash scripts/imputation/ETTh1_imp.sh
+bash scripts/imputation/ETTh1_imp.sh
 bash scripts/imputation/ETTm1_imp.sh
-bash scripts/imputation/Weather_imp.sh
-# bash scripts/imputation/ECL_imp.sh
+#bash scripts/imputation/Weather_imp.sh
+bash scripts/imputation/ECL_imp.sh
--- models/FEDformer.py
+++ models/FEDformer.py
@@ -133,14 +133,14 @@ class Model(nn.Module):
 
     def imputation(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask):
         # Normalization from Non-stationary Transformer
-        means = torch.sum(x_enc, dim=1) / torch.sum(mask == 1, dim=1)
-        means = means.unsqueeze(1).detach()
-        x_enc = x_enc - means
-        x_enc = x_enc.masked_fill(mask == 0, 0)
-        stdev = torch.sqrt(torch.sum(x_enc * x_enc, dim=1) /
-                           torch.sum(mask == 1, dim=1) + 1e-5)
-        stdev = stdev.unsqueeze(1).detach()
-        x_enc /= stdev
+        # means = torch.sum(x_enc, dim=1) / torch.sum(mask == 1, dim=1)
+        # means = means.unsqueeze(1).detach()
+        # x_enc = x_enc - means
+        # x_enc = x_enc.masked_fill(mask == 0, 0)
+        # stdev = torch.sqrt(torch.sum(x_enc * x_enc, dim=1) /
+        #                    torch.sum(mask == 1, dim=1) + 1e-5)
+        # stdev = stdev.unsqueeze(1).detach()
+        # x_enc /= stdev
         
         # enc
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
@@ -149,12 +149,12 @@ class Model(nn.Module):
         dec_out = self.projection(enc_out)
         
         # De-Normalization from Non-stationary Transformer
-        dec_out = dec_out * \
-                  (stdev[:, 0, :].unsqueeze(1).repeat(
-                      1, self.pred_len + self.seq_len, 1))
-        dec_out = dec_out + \
-                  (means[:, 0, :].unsqueeze(1).repeat(
-                      1, self.pred_len + self.seq_len, 1))
+        # dec_out = dec_out * \
+        #           (stdev[:, 0, :].unsqueeze(1).repeat(
+        #               1, self.pred_len + self.seq_len, 1))
+        # dec_out = dec_out + \
+        #           (means[:, 0, :].unsqueeze(1).repeat(
+        #               1, self.pred_len + self.seq_len, 1))
         return dec_out
 
     def anomaly_detection(self, x_enc):
