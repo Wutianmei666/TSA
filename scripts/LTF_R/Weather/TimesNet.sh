ede880082432fe880a6899f32297fa34195834b4
XiAnServer3 <1461304613@qq.com>
2024-10-04 20:15:32 +0800

    update

--- scripts/LTF_R/Weather/TimesNet.sh
+++ scripts/LTF_R/Weather/TimesNet.sh
@@ -1,4 +1,5 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
+seed=2024
 model_name=TimesNet
 seq_len=96
 label_len=48
@@ -8,11 +9,12 @@ d_layers=1
 d_model=32
 d_ff=32
 top_k=5
-for interpolate in no mean nearest linear
+for interpolate in mean nearest linear
 do
     for mask_rate in  0.125 0.25 0.375 0.5 0.625 0.75
     do
         python -u mix_run.py \
+          --random_seed $seed \
           --task_name long_term_forecast \
           --train_mode 2 \
           --mask_rate $mask_rate \
@@ -22,6 +24,7 @@ do
           --root_path ./dataset/weather/ \
           --data_path weather.csv \
           --model $model_name \
+          --dataset Weather \
           --data custom \
           --features M \
           --seq_len $seq_len \
